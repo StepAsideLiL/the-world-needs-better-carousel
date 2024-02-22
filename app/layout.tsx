1aafdf76bc6622ce1a0ba45c26bd078f4e628c02
Rifat Khan <rifat6633@gmail.com>
2024-02-22 19:41:53 +0600

    add: dark mode support

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Link from "next/link";
 import { Square } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import Providers from "@/components/providers/providers";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,21 +24,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="container py-5 flex items-center gap-3">
-          <Link href={"/"} className="flex items-center">
-            <Square className="size-5 text-muted-foreground" />
-            <Square className="size-7" />
-            <Square className="size-5 text-muted-foreground" />
-          </Link>
+        <Providers>
+          <header className="container py-5 flex justify-between items-center">
+            <div className="flex items-center gap-3">
+              <Link href={"/"} className="flex items-center">
+                <Square className="size-5 text-muted-foreground" />
+                <Square className="size-7" />
+                <Square className="size-5 text-muted-foreground" />
+              </Link>
 
-          {headerMenus.length !== 0 && (
-            <Separator orientation="vertical" className="h-10" />
-          )}
+              {headerMenus.length !== 0 && (
+                <Separator orientation="vertical" className="h-10" />
+              )}
 
-          <nav></nav>
-        </header>
+              <nav></nav>
+            </div>
 
-        {children}
+            <ModeToggle />
+          </header>
+
+          {children}
+        </Providers>
       </body>
     </html>
   );
