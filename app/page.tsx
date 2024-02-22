c443181e31c9e3ec66b34534dbb8f74541aa77ab
Rifat Khan <rifat6633@gmail.com>
2024-02-22 21:28:20 +0600

    update: remove ShadcnHorizontal and add simple banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { ShadcnHorizontal } from "@/components/carousels/shadcn";
-
 export default function Page() {
   return (
     <main className="min-h-screen container">
-      <ShadcnHorizontal />
+      <section className="py-20">
+        <h1 className="text-5xl font-semibold text-center">
+          The World Needs Better Carousel
+        </h1>
+      </section>
     </main>
   );
 }
